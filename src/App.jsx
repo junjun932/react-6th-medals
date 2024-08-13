63a8b80acbf456dc7e7497f91865ce378388e3b9
junjun932 <1004ji93@gmail.com>
2024-08-13 11:25:04 +0900

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="container">
+      <h1>2024 파리 올림픽 메달 트래커</h1>
+      <form className="input-group">
+        {/* 국가 이름, 메달 갯수의 입력 필드와 버튼 위치 */}
+      </form>
+      <div>{/* 메달 집계 리스트가 표시될 자리 */}</div>
+    </div>
+  );
 }
 
 export default App;
