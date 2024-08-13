06cf61ec1014ea351cd6f5070d390b60c84079a1
junjun932 <1004ji93@gmail.com>
2024-08-13 12:26:31 +0900

    3단계

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,63 @@
+import { useState } from "react";
+
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("");
+  const [gold, setGold] = useState(0);
+  const [silver, setSilver] = useState(0);
+  const [bronze, setBronze] = useState(0);
+
+  console.log(countries);
+  function onSubmit(event) {
+    event.preventDefault();
+
+    console.log(country, gold, silver, bronze);
+    setCountries([
+      {
+        country,
+        gold,
+        silver,
+        bronze,
+      },
+      ...countries,
+    ]);
+
+    setCountry("");
+    setGold(0);
+    setSilver(0);
+    setBronze(0);
+  }
+
   return (
     <div className="container">
       <h1>2024 파리 올림픽 메달 트래커</h1>
-      <form className="input-group">
-        {/* 국가 이름, 메달 갯수의 입력 필드와 버튼 위치 */}
+      <form className="input-group" onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="country"
+          value={country}
+          onChange={(event) => setCountry(event.target.value)}
+        ></input>
+        <input
+          type="number"
+          name="gold"
+          value={gold}
+          onChange={(event) => setGold(event.target.value)}
+        ></input>
+        <input
+          type="number"
+          name="silver"
+          value={silver}
+          onChange={(event) => setSilver(event.target.value)}
+        ></input>
+        <input
+          type="number"
+          name="bronze"
+          value={bronze}
+          onChange={(event) => setBronze(event.target.value)}
+        ></input>
+        <button type="submit">국가추가</button>
+        <button type="button">업데이트</button>
       </form>
       <div>{/* 메달 집계 리스트가 표시될 자리 */}</div>
     </div>
